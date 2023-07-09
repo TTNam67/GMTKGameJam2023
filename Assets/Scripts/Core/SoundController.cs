d9c3ac3c9cde1ddbc69435773616e5c7327f9800
ThanhNam <namwizard67@gmail.com>
2023-07-09 18:31:38 +0700

    First version of the game

--- Assets/Scripts/Core/SoundController.cs
+++ Assets/Scripts/Core/SoundController.cs
@@ -1,21 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
     AudioSource _audioSource;
+    [SerializeField] AudioClip _audioClip;
     void Start()
     {
         _audioSource = GetComponent<AudioSource>();
         if (_audioSource == null)
             Debug.LogWarning("SoundController.cs: AudioSource is not found");
 
+        // _audioSource.Stop();
         _audioSource.volume = 1.0f;
+        // _audioSource.loop = true;
         _audioSource.Play();
 
+        // _audioSource.PlayOneShot(_audioClip);
+
         DontDestroyOnLoad(this.gameObject);
-        
+
+        SceneManager.LoadScene(1);
     }
 
     
@@ -23,6 +30,9 @@ public class SoundController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
     }
 }
